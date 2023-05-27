f05f79f218d2511a69bf5926d9b67e3c3cf56920
Alpha <alphayks@gmail.com>
2023-05-27 15:06:45 -0400

    2nd commit from my personal Dell Latitude laptop

--- dynamodb_tb.tf
+++ dynamodb_tb.tf
@@ -1,10 +1,10 @@
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "terraform-up-and-running-locks"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "new-terraform-up-and-running-state"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
