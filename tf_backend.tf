da2dc0a96c4136e4610261ac2df4a28177407004
Alpha <alphayks@gmail.com>
2023-05-27 16:54:58 -0400

    Commit after migrating the tf state back to remote

--- tf_backend.tf
+++ tf_backend.tf
@@ -1,14 +1,14 @@
-terraform {
-  backend "s3" {
+# terraform {
+#   backend "s3" {
 
-    # Replace this with your bucket name!
-    bucket  = "new-terraform-up-and-running-state"
-    key     = "global/s3/terraform.tfstate"
-    region  = "us-east-2"
-    profile = "alpha-aws"
+#     # Replace this with your bucket name!
+#     bucket  = "new-terraform-up-and-running-state"
+#     key     = "global/s3/terraform.tfstate"
+#     region  = "us-east-2"
+#     profile = "alpha-aws"
 
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "new-terraform-up-and-running-state" 
-    encrypt        = true
-  }
-}
+#     # Replace this with your DynamoDB table name!
+#     dynamodb_table = "new-terraform-up-and-running-state" 
+#     encrypt        = true
+#   }
+# }
